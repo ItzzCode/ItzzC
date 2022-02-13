776233bdaff841acabb63e721c9d5c524d8c240d
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2022-02-12 22:28:30 -0500

    the files

---
+++ ItzzC/src/main.c
@@ -0,0 +1,15 @@
+/*
+ *--------------------------------------
+ * Program Name: ItzzC
+ * Author: Itzz-Me
+ * License: GPL 3.0
+ * Description: something
+ *--------------------------------------
+*/
+
+#include "menu.c"
+
+int main( void ){
+    menu();
+    return 0;
+}
