9c3fb3bc33c468d8693ef9066f562ecfefe6bb50
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2022-02-17 12:35:27 -0500

    Update main.c
    i give up

--- src/main.c
+++ src/main.c
@@ -8,17 +8,30 @@
 */
 
 #include <tice.h>
+#include <keypadc.h>
 #include <graphx.h>
+#include <debug.h>
 
 int main( void ){
-    gfx_Begin();
-    gfx_SetDrawBuffer();
+    kb_key_t key;
+    int curr_color = 255;
+    dbg_sprintf(dbgout, "hey\n");
 
-    while ( !os_GetCSC() ){
-        gfx_FillScreen(255);
-        
+    gfx_Begin();
+
+    do{
+        dbg_sprintf(dbgout, "ho\n");
+        kb_Scan();
+        key = kb_Data[7];
+
+        gfx_FillScreen(curr_color);
+
         gfx_SwapDraw();
-    }
+
+        if ( kb_Data[1] == kb_2nd ){
+            curr_color--;
+        }
+    } while ( kb_Data[1] != kb_Clear );
 
     gfx_End();
     return 0;
